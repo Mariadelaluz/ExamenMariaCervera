3b3af9ad2146b2d36f178ac888c3784ed9f2ab7c
Mariadelaluz <77519306+Mariadelaluz@users.noreply.github.com>
2021-02-21 20:14:28 -0600

    Add files via upload

---
+++ Ej1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n;
+                Console.WriteLine("Ingresa el valor");
+                n = int.Parse(Console.ReadLine());
+                
+                Console.WriteLine("El resultado es: " + Fib(n));
+                Console.ReadLine();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error " + ex.Message);
+            }
+           
+        }
+
+        public static int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+
+    }
+}
